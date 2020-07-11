3a6be193205d3e72f28248bfbb05c46179beddb4
Scott <wpscott@users.noreply.github.com>
2020-07-11 10:48:53 -0400

    update gift display

--- AcFunDanmu/Models/Client/GiftList.cs
+++ AcFunDanmu/Models/Client/GiftList.cs
@@ -1,4 +1,6 @@
-﻿namespace AcFunDanmu.Models.Client
+﻿using System;
+
+namespace AcFunDanmu.Models.Client
 {
     public struct GiftList
     {
@@ -15,7 +17,20 @@
                 public int giftId { get; set; }
                 public string giftName { get; set; }
                 public int giftPrice { get; set; }
+                public Pic[] pngPicList { get; set; }
+                public Pic[] webpPicList { get; set; }
+                public struct Pic
+                {
+                    public string cdn { get; set; }
+                    public string url { get; set; }
+                }
             }
         }
+    }
+
+    public struct GiftInfo
+    {
+        public string Name { get; set; }
+        public Uri Pic { get; set; }
     }
 }
