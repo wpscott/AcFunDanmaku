e8aaeea0598210ec641bfc0b31ce808a582dacf6
Scott <wpscott@users.noreply.github.com>
2020-12-14 15:10:24 -0500

    Update Play.cs

--- AcFunDanmu/Models/Client/Play.cs
+++ AcFunDanmu/Models/Client/Play.cs
@@ -2,7 +2,7 @@
 {
     public sealed record Play
     {
-        public int result { get; init; }
+        public int result { get; init; }    // 129004: LiveNotOpen, 129015 UserBanned, 380205 LiveNotPaid
         public int error_code { get; init; }
         public string error_msg { get; init; }
         public PlayData data { get; init; }
